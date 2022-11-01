b0f277f1ab36302e79c1edf2bb3c36530e0d32f5
Paula Flávia Pagotto Simionato <paulaflaviapagottosimionato@MacBook-Air-de-Paula.local>
2022-11-01 14:51:32 -0300

    conflito resolvido

--- routers/UsuariosRouter.js
+++ routers/UsuariosRouter.js
@@ -3,11 +3,11 @@ const UsuariosController = require('../Controllers/UsuariosController');
 
 const UsuariosRouter = express.Router();
 
-UsuariosRouter.get('/', UsuariosController.index);
-UsuariosRouter.get('/create', UsuariosController.create);
-UsuariosRouter.post('/store', UsuariosController.store);
-UsuariosRouter.get('/:id/edit', UsuariosController.edit);
-UsuariosRouter.post('/:id/update', UsuariosController.update);
-UsuariosRouter.post('/:id/delete', UsuariosController.delete);
+UsuariosRouter.get('/',UsuariosController.index);
+UsuariosRouter.get('/create',UsuariosController.create);
+UsuariosRouter.post('/store',UsuariosController.store);
+UsuariosRouter.get('/:id/edit',UsuariosController.edit);
+UsuariosRouter.post('/:id/update',UsuariosController.update);
+UsuariosRouter.post('/:id/delete',UsuariosController.delete);
 
 module.exports = UsuariosRouter;
